34ce164bb4931dfeddec837de73481f069b7a2d9
IsaiasBritto <isaiassb@gmail.com>
2026-07-28 18:02:10 -0300

    Update locals.tf

--- aulas/02-storage-bancos/lab/terraform/locals.tf
+++ aulas/02-storage-bancos/lab/terraform/locals.tf
@@ -1,4 +1,5 @@
 locals {
   location_sql    = coalesce(var.location_sql, var.location)
   location_search = coalesce(var.location_search, var.location)
+  location_aci    = coalesce(var.location_aci, var.location)
 }
