d0848ea89d9301d3db1a6c8d7a28c8b94d2ee8cc
IsaiasBritto <isaiassb@gmail.com>
2026-07-28 19:21:19 -0300

    Add image_prefix local variable based on registry_server

--- aulas/02-storage-bancos/lab/terraform/locals.tf
+++ aulas/02-storage-bancos/lab/terraform/locals.tf
@@ -2,4 +2,6 @@ locals {
   location_sql    = coalesce(var.location_sql, var.location)
   location_search = coalesce(var.location_search, var.location)
   location_aci    = coalesce(var.location_aci, var.location)
+  image_prefix    = var.registry_server == null ? "" : "${var.registry_server}/"
 }
+
